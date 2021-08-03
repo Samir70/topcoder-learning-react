1858b0ddd8ea1b9cee60d6cf972b3726317b9b25
Samir70 <sbetmouni@talktalk.net>
2021-08-03 12:04:42 +0100

    add changing state so ores can be entered

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { useState } from "react";
 import MineSiteInput from "./components/MineSiteInput";
 
 import "./App.css";
 
 function App() {
+  const [names, setNames] = useState([""]);
   return (
     <div className="app">
-      <MineSiteInput/>
+      <MineSiteInput names={names} update={setNames} />
+      <p>Names has length {names.length} and is {names.join(',')}</p>
     </div>
   );
 }
--- src/components/MineSiteInput/index.jsx
+++ src/components/MineSiteInput/index.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
-
 import "./styles.css";
 
-function MineSiteInput() {
-  const [names] = useState([""]);
-
+function MineSiteInput(props) {
+  const handleChange = (i, event) => {
+    let newNames = [...props.names];
+    newNames[i] = event.target.value
+    props.update([...newNames])
+  }
   return (
     <div className="mine-site-input">
       Mine Site Input
-      {names.map((val, index) => (
+      {props.names.map((val, index) => (
         <div key={index}>
           <div>ore {index + 1}</div>
           <input
             type="text"
+            value={val}
+            placeholder={`name of ore ${index+1}`}
+            onChange={(e) => handleChange(index, e)}
             id={`ore${index + 1}`}
           />
         </div>
       ))}
-    </div>
+      <button id="addMoreButton" onClick={() => props.update([...props.names, ""])}>Add another ore</button>
+    </div>  
   );
 }
 
