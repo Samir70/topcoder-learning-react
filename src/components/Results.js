2ea797595e7754f97c57db285c09801c8278b62b
Samir70 <sbetmouni@talktalk.net>
2021-09-05 11:14:41 +0100

    hide back to process button if last ore finished

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getResults } from '../Redux/selectors';
+import { getResults, checkIfLastOreFinished } from '../Redux/selectors';
 import { GoToMiningProcessButton } from './NavButtons';
 
 export const Results = () => {
     const miningPlan = useSelector(getResults);
+    const lastOreFinished = useSelector(checkIfLastOreFinished);
     const res = miningPlan.map((ore, i) =>
         <tr key={i}>
             <td>{ore.name}</td>
@@ -26,7 +27,7 @@ export const Results = () => {
                     {res}
                 </tbody>
             </table>
-            <GoToMiningProcessButton />
+            {lastOreFinished ? <p>No more ores to process</p> : <GoToMiningProcessButton />}
         </div>
     )
 }
