b5f9435bd3ee8b91d59a9f110be7062e4bc4bf7f
Samir70 <sbetmouni@talktalk.net>
2021-09-05 11:04:35 +0100

    display results in a table

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,11 +5,22 @@ import { GoToMiningProcessButton } from './NavButtons';
 
 export const Results = () => {
     const miningPlan = useSelector(getResults);
-    const res =  miningPlan.map((ore, i) => <p key={i}>Mined {ore.amount} of {ore.name}</p>)
+    const res = miningPlan.map((ore, i) => <tr key={i}><td>{ore.name}</td><td>{ore.amount}</td><td>??</td></tr>)
     return (
         <div>
             <h1>The Mining results</h1>
-            {res}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Ore Name</th>
+                        <th>Amount Mined</th>
+                        <th>Total Jobs</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {res}
+                </tbody>
+            </table>
             <GoToMiningProcessButton />
         </div>
     )
